ebf2cab8f3445740c34db7dc5809984e4e1c04a4
Vovan-VE <vovan-ve@yandex.ru>
2016-06-17 09:35:03 +0800

    WIP: add test cases

---
+++ tests/unit/common/TokenTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace VovanVE\parser\tests\unit\common;
+
+use VovanVE\parser\common\Token;
+use VovanVE\parser\tests\helpers\BaseTestCase;
+
+class TokenTest extends BaseTestCase
+{
+    public function testDumpAsString()
+    {
+        $type = 'foo';
+        $content = 'bar';
+
+        $token = new Token();
+        $token->type = $type;
+        $token->content = $content;
+
+        $dump_end = " `- $type <$content>" . PHP_EOL;
+
+        foreach (['', '    ', "\t"] as $indent) {
+            foreach ([false, true] as $last) {
+                $this->assertEquals(
+                    $indent . $dump_end,
+                    $token->dumpAsString($indent, $last),
+                    'indent=' . json_encode($indent) . ', last=' . ($last ? 'true' : 'false')
+                );
+            }
+        }
+    }
+}
